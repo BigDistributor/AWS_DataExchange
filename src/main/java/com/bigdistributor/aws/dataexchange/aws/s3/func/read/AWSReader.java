5dfd80bc1ce5c7a9283055f84d096bf57e969153
Marwan <zouinkhi.marwan@gmail.com>
2021-04-16 10:12:03 +0100

    reader with uri

--- src/main/java/com/bigdistributor/aws/dataexchange/aws/s3/func/read/AWSReader.java
+++ src/main/java/com/bigdistributor/aws/dataexchange/aws/s3/func/read/AWSReader.java
@@ -1,8 +1,9 @@
 package com.bigdistributor.aws.dataexchange.aws.s3.func.read;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3URI;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.bigdistributor.aws.dataexchange.aws.s3.func.bucket.S3BucketInstance;
 import com.google.common.io.CharStreams;
 
 import java.io.IOException;
@@ -11,20 +12,20 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class AWSReader {
-    protected final S3BucketInstance bucketInstance;
-    protected final String path;
-    protected final String fileName;
+//    protected final S3BucketInstance bucketInstance;
+    protected final String uri;
+    protected final AmazonS3 s3;
 
-    public AWSReader(S3BucketInstance bucketInstance, String path, String fileName) {
-        this.bucketInstance = bucketInstance;
-        this.path = path;
-        this.fileName = fileName;
+    public AWSReader(AmazonS3 s3, String uri) {
+        this.s3 = s3;
+        this.uri = uri;
     }
 
     public String get() throws IOException {
-        GetObjectRequest request = new GetObjectRequest(bucketInstance.getBucketName(), path + fileName);
+        AmazonS3URI amazonS3URI = new AmazonS3URI(uri);
+        GetObjectRequest request = new GetObjectRequest(amazonS3URI.getBucket(),amazonS3URI.getKey());
         System.out.println("Getting file: "+request.getKey() + " from bucket "+ request.getBucketName());
-        S3Object object = bucketInstance.getS3().getObject(request);
+        S3Object object = s3.getObject(request);
         InputStream objectData = object.getObjectContent();
         String text;
         try (Reader reader = new InputStreamReader(objectData)) {
